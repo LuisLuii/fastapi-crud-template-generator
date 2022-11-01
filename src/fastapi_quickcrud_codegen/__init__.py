a9f277cb43c4d880e152533296eb5d316b00c9da
Luis <luis11235178@gmail.com>
2022-11-01 23:56:47 +0800

    Revert "Feature/crud project gen (#1)"
    This reverts commit e8f636a5ce0d1a22ab3511e9f27eb2d8eb5c664b.

--- src/fastapi_quickcrud_codegen/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils import sqlalchemy_to_pydantic
-from .crud_generator import crud_router_builder
-from .misc.type import CrudMethods
-
-
--- src/fastapi_quickcrud_codegen/utils/is_table.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Table
-
-
-def is_table(db_model):
-    if isinstance(db_model, Table):
-        return True
-    return False
